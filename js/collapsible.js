b079ac30ddc6a4f34118768f992b83a38b1e69f5
Jeffery Quattrociocchi <ciaranquattro@gmail.com>
2025-04-15 08:44:34 -0400

    collapsible sidebar

---
+++ js/collapsible.js
@@ -0,0 +1,9 @@
+var coll = document.getElementsByClassName("collapse-button");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("open");
+  });
+}
